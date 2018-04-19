b4050c6612817effc9e08564736bba7cb279ce6d
Sam Moffat <sam.moffat@hotmail.com>
2018-04-19 14:42:07 +0100

    done additional work to review HTML formatting of JSON response.

--- html/js/review.js
+++ html/js/review.js
@@ -20,8 +20,29 @@ form.addEventListener("submit", function (e) {
     data: fData,
     async: true,
     success: function (data) {
-    	console.log(data);
-    	subRes = JSON.parse(data);
+    	var subRes = JSON.parse(data);
+			for (i =0; i<subRes.length; i++) {
+				$("#response-tables").append('<thead>' +
+			    															'<tr>' +
+																			    '<th scope="col">Python used?</th>' +
+																					'<th scope="col">Resolution</th>' +
+																			    '<th scope="col">Combi</th>' +
+																			    '<th scope="col">Multi</th>' +
+																					'<th scope="col">Waters</th>' +
+																			    '<th scope="col">Threed</th>' +
+																			    '<th scope="col">Confs<th>' +
+																			    '<th scope="col">Freq</th>' +
+																					'<th scope="col">Step</th>' +
+																			    '<th scope="col">D. Step</th>' +
+																			    '<th scope="col">Keep List</th>' +
+																			    '<th scope="col">Modes</th>' +
+																					'<th scope="col">Cutoffs</th>' +
+																		  	'</tr>' +
+																			'</thead')
+															.append('<p>test</p>');
+
+				console.log(subRes[i]);
+			};
     },
     cache: false,
     contentType: false,
