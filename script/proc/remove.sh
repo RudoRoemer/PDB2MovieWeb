e6aef50639ddaf1c40bd6e750993e65b4a087687
Samuel Moffat <phsbqz@godzilla.csc.warwick.ac.uk>
2018-04-26 11:19:47 +0100

    drastically reduced search time for job ID. Extremely low collision chance, but not 100% like last method. May need to rollback if this proves to be a problem.

--- script/proc/remove.sh
+++ script/proc/remove.sh
@@ -3,21 +3,6 @@
 purename=$1
 processingUser="phsbqz"
 
-wholeEntry=$(qstat -x | xmllint --format - | xml_grep "Job" --text_only | grep $processingUser | grep $purename | sed 's/\(.\)/\1\n/g')
-
-arr=($wholeEntry)
-echo $arr
-
-tmp1=""
-tmp2=""
-for i in "${arr[@]}"
-do
-	tmp1=$tmp2
-	tmp2=$i
-	curText=${curText}$i
-	echo $curText
-	if [ "$tmp1$tmp2" == ".c" ]; then
-		qdel ${curText%.*}
-		exit	
-	fi
-done
+wholeEntry=$(qstat -a | grep $processingUser | grep ${purename::16} | sed 's/\.moo.*/.moo/')
+echo $wholeEntry
+qdel $wholeEntry
