37b6a313f207bd2349bc57e4e9cda7684642fa73
Sam Moffat <sam.moffat@hotmail.com>
2018-05-21 15:53:15 +0100

    Updated dailyClean to better fit the current version of the project.

--- script/web/dailyClean.sh
+++ script/web/dailyClean.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-remoteUser=phsbqz
-remoteServer=godzilla.csc.warwick.ac.uk
-log="/var/www/html/TEST.txt"
-
-echo "This ran $(date), files delete:">>$log
-
-for file in ./download/* ; do
-        if [[ $(find "$file" -mtime +7 -print) ]]; then
-                echo '    '$file>>$log
-                rm $file
-        fi
-done
-
-ssh $remoteUser@$remoteServer 'cd /storage/disqs/phsbqz && ./garbageCollection.sh'
-
-mysql -u admin -p'penicillin_loves_beta_lactamase' -D db_pdb2movie -e"INSERT INTO RequestsHistory SELECT * FROM Requests;DELETE FROM Requests WHERE filename='$purename';UPDATE Users SET current_requests = 0"
+source "../../../configs.conf"
+find $localScripts/download/* -mtime +7 -exec rm {} \;
+mysql -u $sqlUser -p"$sqlPass" -D $sqlDB -e"UPDATE Users SET current_requests = 0;"
