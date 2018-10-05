efcbed2657fe84eb1f43ac2bd6348871e58e840e
Rudo Roemer <RudoRoemer@web.de>
2018-10-05 19:32:48 +0100

    complete reformtting of all .php files

--- html/php/PdbChecker.php
+++ html/php/PdbChecker.php
@@ -1,51 +1,52 @@
 <?php
 
-  /*
-    checker for the pdb file. This will check for any illegal chars within the file.
-    An exception is given to the REMARK command, as this is treated as regular text.
-  */
+/*
+  checker for the pdb file. This will check for any illegal chars within the file.
+  An exception is given to the REMARK command, as this is treated as regular text.
+*/
 
-  class PdbChecker extends FileChecker {
-
+class PdbChecker extends FileChecker {
+    
     public function __construct($uniqueFileName) {
-
-      $this->hashedName = $uniqueFileName;
-      $this->file = "pdbFile";
-      $this->mimeTypes = array("chemical/x-pdb", "text/plain");
-      $this->ext = "pdb";
-      return $this->check();
-
+        
+        $this->hashedName = $uniqueFileName;
+        $this->file = "pdbFile";
+        $this->mimeTypes = array("chemical/x-pdb", "text/plain");
+        $this->ext = "pdb";
+        return $this->check();
+        
     }
-
+    
     public function check() {
-
-      $this->baseCheck();
-
-      if ($this->checkRes === "Success") {
-
-        //split text lines in file into array and loop
-      $file=file_get_contents( $_FILES[$this->file]['tmp_name'] /*$this->getTmpLocation()*/ );
-        $remove = "\n";
-        $split = explode($remove, $file);
-        $lCount = 1;
-        foreach ($split as $str) {
-
-          //checks each line for unauthorised chars. remark statements are always treated as text and can have special characters in them
-          $san = filter_var($str, FILTER_SANITIZE_SPECIAL_CHARS);
-          if ($san !== $str && substr($str,0,6) !== "REMARK" && strpos($str, "'") !== false) {
-            if (strpos($str, "'") !== false && (strpos($str, "HETNAM") === false AND strpos($str, "HETATM") === false)) {
-              $hasPos = strpos($str, "'");
-              $isHet = strpos($str, "HETNAM");
-              $this->checkRes = sprintf(". At least one unexpected character found at line %s of .pdb file. hasApos: %s isHet: %s str: %s", $lCount,  $hasPos, $isHet, $str);
-              break;
+        
+        $this->baseCheck();
+        
+        if ($this->checkRes === "Success") {
+            
+            //split text lines in file into array and loop
+            $file=file_get_contents( $_FILES[$this->file]['tmp_name'] /*$this->getTmpLocation()*/ );
+            $remove = "\n";
+            $split = explode($remove, $file);
+            $lCount = 1;
+            foreach ($split as $str) {
+                
+                // checks each line for unauthorised chars.
+                // remark statements are always treated as text and can have special characters in them
+                $san = filter_var($str, FILTER_SANITIZE_SPECIAL_CHARS);
+                if ($san !== $str && substr($str,0,6) !== "REMARK" && strpos($str, "'") !== false) {
+                    if (strpos($str, "'") !== false && (strpos($str, "HETNAM") === false AND strpos($str, "HETATM") === false)) {
+                        $hasPos = strpos($str, "'");
+                        $isHet = strpos($str, "HETNAM");
+                        $this->checkRes = sprintf(". At least one unexpected character found at line %s of .pdb file. hasApos: %s isHet: %s str: %s", $lCount,  $hasPos, $isHet, $str);
+                        break;
+                    }
+                }
+                
+                //returns the line the illegal character appeared on.
+                $lCount++;/**/
+                
             }
-          }
-
-          //returns the line the illegal character appeared on.
-          $lCount++;/**/
-
         }
-      }
     }
-  }
+}
 ?>
--- html/php/TclChecker.php
+++ html/php/TclChecker.php
@@ -1,80 +1,80 @@
 <?php
 
-  class TclChecker extends FileChecker {
-
+class TclChecker extends FileChecker {
+    
     public function __construct($uniqueFileName) {
-
-      $this->hashedName = $uniqueFileName;
-      $this->file = "pyFile";
-      $this->mimeTypes = array("application/x-python-code", "text/x-python",  "text/plain");
-      $this->ext = "py";
-      return $this->check();
-
+        
+        $this->hashedName = $uniqueFileName;
+        $this->file = "pyFile";
+        $this->mimeTypes = array("application/x-python-code", "text/x-python",  "text/plain");
+        $this->ext = "py";
+        return $this->check();
+        
     }
-
+    
     public function check() {
-
-      global $configs;
-      $this->baseCheck();
-
-      if ($this->checkRes === "Success") {
-
-        $file=file_get_contents($_FILES[$this->file]['tmp_name']);
-        $comp=file_get_contents("../../tcl-whitelist.txt");
-        $bloc=file_get_contents("../../tcl-blacklist.txt");
-        $remove = "\n";
-        $split = explode($remove, $file);
-        $csplt = explode($remove, $comp);
-        $bsplt = explode($remove, $bloc);
-        $acc = 0;
-        $blackListFlag = false;
-        $match = true;
-
-        foreach ($split as $str) {
-
-          acc++;
-
-          if (substr($str, 0, 1) === "#" || $str == "") {
-            continue 1;
-          }
-
-          if ($configs["useBlackList"] === "1") {
-            foreach($bsplt as $bl) {
-
-              $bl = preg_replace("/[^a-zA-Z0-9(),;:._# ]+/", "", $bl);
-
-
-              if (strpos($str, $bl) !== false ) {
-
-                $blackListFlag = true;
-                break 2;
-
-              }
-
+        
+        global $configs;
+        $this->baseCheck();
+        
+        if ($this->checkRes === "Success") {
+            
+            $file=file_get_contents($_FILES[$this->file]['tmp_name']);
+            $comp=file_get_contents("../../tcl-whitelist.txt");
+            $bloc=file_get_contents("../../tcl-blacklist.txt");
+            $remove = "\n";
+            $split = explode($remove, $file);
+            $csplt = explode($remove, $comp);
+            $bsplt = explode($remove, $bloc);
+            $acc = 0;
+            $blackListFlag = false;
+            $match = true;
+            
+            foreach ($split as $str) {
+                
+                acc++;
+                
+                if (substr($str, 0, 1) === "#" || $str == "") {
+                    continue 1;
+                }
+                
+                if ($configs["useBlackList"] === "1") {
+                    foreach($bsplt as $bl) {
+                        
+                        $bl = preg_replace("/[^a-zA-Z0-9(),;:._# ]+/", "", $bl);
+                        
+                        
+                        if (strpos($str, $bl) !== false ) {
+                            
+                            $blackListFlag = true;
+                            break 2;
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                if ($configs["useWhiteList"] === "1") {
+                    
+                    foreach($csplt as $wl) {
+                        
+                        $tmp = strpos($wl, "()");
+                        $wl = preg_replace("/[^a-zA-Z0-9,;:._ ]+/", "", $wl);
+                        if (strpos($str, $wl) !== false && $wl !== null) { $match = true; break 1; }
+                        
+                    }
+                    
+                    if (!$match || $blackListFlag) {
+                        $this->checkRes = "command at line " . --$acc . " is black listed, reads:\n\n" . $split[$acc];
+                    }
+                    
+                }
             }
-
-          }
-
-          if ($configs["useWhiteList"] === "1") {
-
-            foreach($csplt as $wl) {
-
-              $tmp = strpos($wl, "()");
-              $wl = preg_replace("/[^a-zA-Z0-9,;:._ ]+/", "", $wl);
-              if (strpos($str, $wl) !== false && $wl !== null) { $match = true; break 1; }
-
-            }
-
-            if (!$match || $blackListFlag) {
-              $this->checkRes = "command at line " . --$acc . " is black listed, reads:\n\n" . $split[$acc];
-            }
-
-          }
         }
-      }
-
+        
     }
-
-  }
+    
+}
 
 ?>
